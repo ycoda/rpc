98502d7d7690157f84ba8f7ee5c209fb264b11c1
ycoda <tankaku@gmail.com>
2025-05-07 11:33:31 +0900

    add: created floor and nroot method

--- client/client.js
+++ client/client.js
@@ -1,24 +1,43 @@
 const net =  require('net');
+const readline = require('readline');
 
-const socketPath = '/tmp/unix.sock';
-const client = net.createConnection(socketPath, () => {
-    client.write(JSON.stringify({
-        method: "subtract",
-        params: [42, 23],
-        param_types: ["int", "int"],
-        id: 1
-    }));
+// readlineインターフェースを作成
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-client.on('data', (data) => {
-    console.log('Received from server:', data.toString());
-    client.end();
-});
+// 質問を順番に処理する関数
+const askQuestion = (query) => new Promise((resolve) => rl.question(query, resolve));
 
-client.on('end', () => {
-    console.log('Disconnected from server');
-});
+const main = async () => {
+    const answer_method = await askQuestion('メソッドを入力してください: ');
+    const answer_params_raw = await askQuestion('パラメータを入力してください: ');
+    rl.close();
 
-client.on('error', (err) => {
-    console.error('Connection error:', err);
-});
+    const answer_params = answer_params_raw.split(" ").map(Number);
+    const answer_param_types = answer_params.map((elm) => {
+        return typeof elm == 'number' ? 'int' : typeof elm
+    });
+    const socketPath = '/tmp/unix.sock';
+    const client = net.createConnection(socketPath, () => {
+        client.write(JSON.stringify({
+            method: answer_method,
+            params: answer_params,
+            param_types: answer_param_types,
+            id: Date.now(),
+        }));
+    });
+
+    client.on('data', (data) => {
+        console.log('Received response:');
+        const response = JSON.parse(data.toString());
+        console.log(JSON.stringify(response, null, 4));
+        client.end();
+    });
+
+    client.on('end', () => console.log('Disconnected from server'));
+    client.on('error', (err) => console.error('Connection error:', err));
+};
+
+main();
